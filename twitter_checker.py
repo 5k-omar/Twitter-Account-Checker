c2491800a48ab7c130d0580541a59bdc95e161b8
5k-omar <92945935+5k-omar@users.noreply.github.com>
2024-12-14 19:25:42 +0200

    Create twitter_checker.py

---
+++ twitter_checker.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+url = "https://discord.gg/A7xSKBX3r7"
+
+webbrowser.open(url)
+
+print("Join Discord Server For Get Src: https://discord.gg/A7xSKBX3r7 (Free)")
+
+input("Press any key to close...")
